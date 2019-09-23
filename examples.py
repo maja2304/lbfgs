f3ab8e7c8854d8f2e6ddef1f0588e78102935468
Marija Radovic <radovic.matf@gmail.com>
2019-09-23 22:02:51 +0200

    Evaluacija izvrsavanja

--- examples.py
+++ examples.py
@@ -1,6 +1,7 @@
 from numpy import *
 from lbfgs import *
 from ctypes import *
+import time
 
 def fun0(x, grad):
 	fx = 0
@@ -55,7 +56,7 @@ OKGREEN = '\033[92m'
 FAIL = '\033[91m'
 
 # Fleg za testiranje [SAMO SA IZBILDOVANOM BIBLIOTEKOM - Pogledati README]
-testing = 0
+testing = 1
 # Kreiranje podrazumevanih parametara
 l = LbfgsClass()
 #l.display = 1
@@ -68,10 +69,13 @@ for i in range(0, N, 2):
 	x[i] = -1.2
 	x[i+1] = 1.0
 
+start = time.monotonic()
 status, fx = l.minimize(N, x, fun0)
+end = time.monotonic()
 if status != 0:
 	print("Greska u pokusaju minimizacije funkcije")
 else:
+	print("Vreme izvrsavanja funkcije sa {} parametara: {} [ms]".format(N, (end - start) * 1000))
 	if testing == 0:
 		print("Rezultat LBFGS minimizacije = ", fx)
 
@@ -82,125 +86,142 @@ if testing == 1:
 	lbfgs_c.restype = c_float
 	c_res = lbfgs_c()
 	print ("Referentna c biblioteka LBFGS = ", c_res)
-	print ("Python implementacija LBFGS= ", fx)
+	print ("Python implementacija LBFGS = ", fx)
 	rel_tol=1e-09
 	if abs(c_res - fx) < rel_tol:
 		print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
 	else :
 		print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
 
-print("--------------------------------------------------")
-print("2. Funkcija")
-# Drugi primer
-N = 2
-x = array([0.0 for i in range(N)])
-for i in range(0, N, 2):
-	x[i] = -1.2
-	x[i+1] = 1.0
-status, fx = l.minimize(N, x, fun1)
-if status != 0:
-	print("Greska u pokusaju minimizacije funkcije")
-else:
-	if testing == 0:
-		print("Rezultat LBFGS minimizacije = ", fx)
+if 0:
+	print("--------------------------------------------------")
+	print("2. Funkcija")
+	# Drugi primer
+	N = 2
+	x = array([0.0 for i in range(N)])
+	for i in range(0, N, 2):
+		x[i] = -1.2
+		x[i+1] = 1.0
 
-# Uporedjivanje sa referentnom C bibliotekom
-if testing == 1:
-	libCalc = CDLL("./libc_to_py.so")
-	lbfgs_c = libCalc.c_lbfgs_1
-	lbfgs_c.restype = c_float
-	c_res = lbfgs_c()
-	print ("Referentna c biblioteka LBFGS = ", c_res)
-	print ("Python implementacija LBFGS= ", fx)
-	rel_tol=1e-09
-	if abs(c_res - fx) < rel_tol:
-		print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
-	else :
-		print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
+	start = time.monotonic()
+	status, fx = l.minimize(N, x, fun1)
+	end = time.monotonic()
+	if status != 0:
+		print("Greska u pokusaju minimizacije funkcije")
+	else:
+		print("Vreme izvrsavanja funkcije: {} [ms]".format((end - start) * 1000))
+		if testing == 0:
+			print("Rezultat LBFGS minimizacije = ", fx)
 
-print("--------------------------------------------------")
-print("3. Funkcija")
-# Treci primer
-N = 2
-x = array([0.0 for i in range(N)])
-for i in range(0, N, 2):
-	x[i] = -1.2
-	x[i+1] = 1.0
-status, fx = l.minimize(N, x, fun2)
-if status != 0:
-	print("Greska u pokusaju minimizacije funkcije")
-else:
-	if testing == 0:
-		print("Rezultat LBFGS minimizacije = ", fx)
+	# Uporedjivanje sa referentnom C bibliotekom
+	if testing == 1:
+		libCalc = CDLL("./libc_to_py.so")
+		lbfgs_c = libCalc.c_lbfgs_1
+		lbfgs_c.restype = c_float
+		c_res = lbfgs_c()
+		print ("Referentna c biblioteka LBFGS = ", c_res)
+		print ("Python implementacija LBFGS = ", fx)
+		rel_tol=1e-09
+		if abs(c_res - fx) < rel_tol:
+			print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
+		else :
+			print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
 
-# Uporedjivanje sa referentnom C bibliotekom
-if testing == 1:
-	libCalc = CDLL("./libc_to_py.so")
-	lbfgs_c = libCalc.c_lbfgs_2
-	lbfgs_c.restype = c_float
-	c_res = lbfgs_c()
-	print ("Referentna c biblioteka LBFGS = ", c_res)
-	print ("Python implementacija LBFGS= ", fx)
-	rel_tol=1e-09
-	if abs(c_res - fx) < rel_tol:
-		print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
-	else :
-		print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
+	print("--------------------------------------------------")
+	print("3. Funkcija")
+	# Treci primer
+	N = 2
+	x = array([0.0 for i in range(N)])
+	for i in range(0, N, 2):
+		x[i] = -1.2
+		x[i+1] = 1.0
 
-print("--------------------------------------------------")
-print("4. Funkcija")
-# Cetvrti primer
-N = 2
-x = array([0.0 for i in range(N)])
-for i in range(0, N, 2):
-	x[i] = -1.2
-	x[i+1] = 1.0
-status, fx = l.minimize(N, x, fun3)
-if status != 0:
-	print("Greska u pokusaju minimizacije funkcije")
-else:
-	if testing == 0:
-		print("Rezultat LBFGS minimizacije = ", fx)
+	start = time.monotonic()
+	status, fx = l.minimize(N, x, fun2)
+	end = time.monotonic()
+	if status != 0:
+		print("Greska u pokusaju minimizacije funkcije")
+	else:
+		print("Vreme izvrsavanja funkcije: {} [ms]".format((end - start) * 1000))
+		if testing == 0:
+			print("Rezultat LBFGS minimizacije = ", fx)
 
-# Uporedjivanje sa referentnom C bibliotekom
-if testing == 1:
-	libCalc = CDLL("./libc_to_py.so")
-	lbfgs_c = libCalc.c_lbfgs_3
-	lbfgs_c.restype = c_float
-	c_res = lbfgs_c()
-	print ("Referentna c biblioteka LBFGS = ", c_res)
-	print ("Python implementacija LBFGS= ", fx)
-	rel_tol=1e-09
-	if abs(c_res - fx) < rel_tol:
-		print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
-	else :
-		print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
+	# Uporedjivanje sa referentnom C bibliotekom
+	if testing == 1:
+		libCalc = CDLL("./libc_to_py.so")
+		lbfgs_c = libCalc.c_lbfgs_2
+		lbfgs_c.restype = c_float
+		c_res = lbfgs_c()
+		print ("Referentna c biblioteka LBFGS = ", c_res)
+		print ("Python implementacija LBFGS = ", fx)
+		rel_tol=1e-09
+		if abs(c_res - fx) < rel_tol:
+			print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
+		else:
+			print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
 
-print("--------------------------------------------------")
-print("5. Funkcija")
-# Peti primer
-N = 2
-x = array([0.0 for i in range(N)])
-for i in range(0, N, 2):
-	x[i] = -1.2
-	x[i+1] = 1.0
-status, fx = l.minimize(N, x, fun4)
-if status != 0:
-	print("Greska u pokusaju minimizacije funkcije")
-else:
-	if testing == 0:
-		print("Rezultat LBFGS minimizacije = ", fx)
+	print("--------------------------------------------------")
+	print("4. Funkcija")
+	# Cetvrti primer
+	N = 2
+	x = array([0.0 for i in range(N)])
+	for i in range(0, N, 2):
+		x[i] = -1.2
+		x[i+1] = 1.0
 
-# Uporedjivanje sa referentnom C bibliotekom
-if testing == 1:
-	libCalc = CDLL("./libc_to_py.so")
-	lbfgs_c = libCalc.c_lbfgs_4
-	lbfgs_c.restype = c_float
-	c_res = lbfgs_c()
-	print ("Referentna c biblioteka LBFGS = ", c_res)
-	print ("Python implementacija LBFGS= ", fx)
-	rel_tol=1e-09
-	if abs(c_res - fx) < rel_tol:
-		print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
-	else :
-		print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
+	start = time.monotonic()
+	status, fx = l.minimize(N, x, fun3)
+	end = time.monotonic()
+	if status != 0:
+		print("Greska u pokusaju minimizacije funkcije")
+	else:
+		print("Vreme izvrsavanja funkcije: {} [ms]".format((end - start) * 1000))
+		if testing == 0:
+			print("Rezultat LBFGS minimizacije = ", fx)
+
+	# Uporedjivanje sa referentnom C bibliotekom
+	if testing == 1:
+		libCalc = CDLL("./libc_to_py.so")
+		lbfgs_c = libCalc.c_lbfgs_3
+		lbfgs_c.restype = c_float
+		c_res = lbfgs_c()
+		print ("Referentna c biblioteka LBFGS = ", c_res)
+		print ("Python implementacija LBFGS = ", fx)
+		rel_tol=1e-09
+		if abs(c_res - fx) < rel_tol:
+			print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
+		else :
+			print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
+
+	print("--------------------------------------------------")
+	print("5. Funkcija")
+	# Peti primer
+	N = 2
+	x = array([0.0 for i in range(N)])
+	for i in range(0, N, 2):
+		x[i] = -1.2
+		x[i+1] = 1.0
+
+	start = time.monotonic()
+	status, fx = l.minimize(N, x, fun4)
+	start = time.monotonic()
+	if status != 0:
+		print("Greska u pokusaju minimizacije funkcije")
+	else:
+		print("Vreme izvrsavanja funkcije: {} [ms]".format((end - start) * 1000))
+		if testing == 0:
+			print("Rezultat LBFGS minimizacije = ", fx)
+
+	# Uporedjivanje sa referentnom C bibliotekom
+	if testing == 1:
+		libCalc = CDLL("./libc_to_py.so")
+		lbfgs_c = libCalc.c_lbfgs_4
+		lbfgs_c.restype = c_float
+		c_res = lbfgs_c()
+		print ("Referentna c biblioteka LBFGS = ", c_res)
+		print ("Python implementacija LBFGS = ", fx)
+		rel_tol=1e-09
+		if abs(c_res - fx) < rel_tol:
+			print(OKGREEN, BOLD, "Tacnost potvrdjena sa C bibliotekom", ENDC)
+		else :
+			print(FAIL, BOLD,"Postoje neslaganje izmedju pajton i c racunice", ENDC)
